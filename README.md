66f8cec48e0483888b07304c88578707a290f31e
Andrea Oliveri <andrea_oliveri@outlook.com>
2022-11-11 22:05:45 +0100

    Add deepface as submodule

--- README.md
+++ README.md
@@ -10,5 +10,10 @@ pip install mtcnn hdbscan
 conda create -n deepface python=3.10 matplotlib pandas
 conda activate deepface
 conda install -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
-pip install tensorflow==2.10 opencv-python==4.5 Pillow pillow-heif
-pip install deepface hdbscan
+pip install tensorflow==2.10 opencv-contrib-python==4.5.5.64 Pillow pillow-heif 
+pip install cmake
+pip install deepface hdbscan 
+
+
+# Optionally
+pip install mediapipe dlib cmake
--- src/extract_faces.py
+++ src/extract_faces.py
@@ -7,6 +7,9 @@ import contextlib
 import image_io
 import utils
 
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'deepface')))
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from deepface.detectors import FaceDetector
 
@@ -36,7 +39,8 @@ def main():
                              n_files, 
                              args.read_videos,
                              args.secs_between_frames,
-                             args.detector_name)
+                             args.detector_name,
+                             args.min_confidence)
 
 
 
@@ -69,6 +73,11 @@ def parse_args():
                         dest = 'detector_name',
                         help = 'The name of the detector used to detect and extract faces from images. Must be supported by deepface.detectors.FaceDetector.')
 
+    parser.add_argument('-c', '--min_confidence', 
+                        type = float,
+                        default = 0.9,
+                        help = 'Minimum confidence score to accept a face detection as valid.')
+
     args = parser.parse_args()
 
     return args
@@ -85,9 +94,9 @@ class DeepfaceDetectorWrapper:
     def detect_faces(self, image):
         with open(os.devnull, 'w') as devnull_stream:
             with contextlib.redirect_stdout(devnull_stream), contextlib.redirect_stderr(devnull_stream):
-                faces_regions = FaceDetector.detect_faces(self.detector, self.detector_name, image, align = False)
+                faces_regions_scores = FaceDetector.detect_faces(self.detector, self.detector_name, image, align = False)
 
-        return faces_regions
+        return faces_regions_scores
 
 
 
@@ -112,7 +121,7 @@ class TSVWriter:
             
 def detect_and_extract_faces(input_dir, output_dir, n_files, 
                              read_videos = False, secs_between_frames = 1, 
-                             detector_name = 'retinaface'):    
+                             detector_name = 'retinaface', min_confidence = 0.9):    
 
     tsv_path = os.path.join(output_dir, "faces.tsv")
     tsv_writer = TSVWriter(tsv_path).write_line('id', 'image_path')
@@ -123,9 +132,12 @@ def detect_and_extract_faces(input_dir, output_dir, n_files,
     
     with tqdm(total = n_files, ascii = True, desc = 'Files processed') as pbar:
         for file_idx, file_path, image in image_io.photos_video_frames_iterator(input_dir, read_videos, secs_between_frames):
-            faces_regions = detector.detect_faces(image)
+            faces_regions_scores = detector.detect_faces(image)
 
-            for face, _ in faces_regions:
+            for face, _, confidence in faces_regions_scores:
+                if confidence < min_confidence:
+                    continue
+
                 image_io.imsave(face, os.path.join(output_dir, f"{patch_id}.png"))
                 tsv_writer.write_line(patch_id, file_path)
                 patch_id += 1
@@ -142,5 +154,3 @@ def detect_and_extract_faces(input_dir, output_dir, n_files,
 
 if __name__ == '__main__':
     main()
-
-
