ca8ca17d2f5b62d53da9f3ef9ce8df2c7cb33238
Andrea Oliveri <andrea_oliveri@outlook.com>
2023-01-08 09:38:44 +0100

    Added auto-rotation to loaded images

--- src/image_io.py
+++ src/image_io.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-from PIL import Image
+from PIL import Image, ImageOps
 import pillow_heif
 pillow_heif.register_heif_opener()
 pillow_heif.register_avif_opener()
@@ -13,49 +13,20 @@ from numpy import asarray
 import os
 os.environ["OPENCV_LOG_LEVEL"] = "SILENT"
 
-from cv2 import VideoCapture, CAP_PROP_FPS, CAP_FFMPEG, imwrite
-
-
-from utils import dir_children_iter
+from cv2 import VideoCapture, CAP_PROP_FPS, CAP_FFMPEG
 
 
 
 
 
-def photos_video_frames_iterator(input_dir, read_videos = False, secs_between_frames = 1):    
-    """
-    
-    image::[np.array]
-        Image or video frame in BGR format.
-    """
-
-
-    for file_idx, file_path in enumerate(dir_children_iter(input_dir)):
-
-        image_load_success, image = _load_image(file_path)
-        
-        if image_load_success:
-            yield file_idx, file_path, image
-        
-        elif read_videos:
-            for frame in _iter_video(file_path, secs_between_frames):
-                yield file_idx, file_path, frame
-
-
-
-def imsave(image, path):
-    imwrite(path, image)
-
-
-
-
-def _load_image(path):
+def load_image(path):
     image_load_success = True
     image = None
 
     try:
-        image = asarray(Image.open(path).convert("RGB"))
-        image = image[..., ::-1]
+        image = Image.open(path)
+        image = ImageOps.exif_transpose(image)
+        image = asarray(image.convert('BGR'))
     except Exception:
         image_load_success = False
 
@@ -64,7 +35,7 @@ def _load_image(path):
 
 
 
-def _iter_video(path, secs_between_frames):
+def iter_video(path, secs_between_frames):
     # Regardless of format, try reading it as a video. If OpenCV fails, it will write to
     # stderr but no exception is launched in Python. Therefore program will keep running
     # without interruptions, and cap.isOpened() will return False. 
