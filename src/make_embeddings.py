d719ea6bd968ff903621dfaba3c9d61eb444cfca
Andrea Oliveri <andrea_oliveri@outlook.com>
2025-04-21 22:27:27 +0200

    Updated make_embeddings

--- src/make_embeddings.py
+++ src/make_embeddings.py
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*-
 
-import os
+
+# Constants is imported first so that it sets up the environment variables.
+import constants
+
+import pathlib
 import argparse
 
-from utils import load_tsv
-
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'deepface')))
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-from deepface import DeepFace
-
 import numpy as np
-import pandas as pd
-
+from deepface import DeepFace
 from tqdm import tqdm
 
+import utils
 
 
 
@@ -23,12 +19,19 @@ from tqdm import tqdm
 def main():
     args = parse_args()
 
+    faces_csv_path       = args.work_dir / constants.FACES_CSV_FILENAME
+    cropped_faces_dir    = args.work_dir / constants.CROPPED_FACES_DIRNAME
+    embeddings_file_path = args.work_dir / constants.EMBEDDINGS_FILENAME
+    
+    if embeddings_file_path.is_file():
+        embeddings_file_path.unlink()
+    
     print('Starting creating face embeddings...')
-    embeddings = make_embeddings(args.input_dir, args.input_faces_tsv_path, args.model, args.normalization)
+    embeddings = make_embeddings(cropped_faces_dir, faces_csv_path, args.model, args.normalization)
 
     print('Saving embeddings...')
     embeddings = np.stack(embeddings, axis = 0)
-    np.save(args.output_file, embeddings, allow_pickle = False)
+    np.save(embeddings_file_path, embeddings, allow_pickle = False)
 
     print('Process completed.')
 
@@ -37,59 +40,37 @@ def main():
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-i', '--input_dir',
+    parser.add_argument('-w', '--work_dir',
                         required = True,
-                        type = os.path.abspath,
-                        help = 'Input directory containing images of extracted faces.')
+                        type = pathlib.Path,
+                        help = 'Output directory from step extract_faces. It contains the extracted faces and the CSV.')
 
-    parser.add_argument('-e', '--input_faces_tsv_path',
-                        required = False,
-                        default = None,
-                        type = os.path.abspath,
-                        help = 'Input file path that stores the (patch_id, original_image_path) pairs in tsv format.')
-
-    parser.add_argument('-o', '--output_file',
-                        default = None,
-                        type = os.path.abspath,
-                        help = 'Output file path that will store array of embeddings for each extracted face. If this argument does not end with .npy, this extension is automatically appended.')
-
-    parser.add_argument('-m', '--model', 
+    parser.add_argument('-m', '--model',
                         default = 'Facenet512',
-                        help = 'The name of the model used to create the face embeddings. Must be supported by deepface.DeepFace.represent.')
+                        help = 'The name of the model used to create the face embeddings. Must be supported by DeepFace.represent().')
 
-    parser.add_argument('-n', '--normalization', 
+    parser.add_argument('-n', '--normalization',
                         default = 'Facenet',
                         help = 'The normalization technique used in the pre-processing step of the face images. Must be supported by deepface.commons.functions.normalize_input.')
 
     args = parser.parse_args()
-
-    # Default for args.input_faces_tsv_path
-    if args.input_faces_tsv_path is None:
-        args.input_faces_tsv_path = os.path.abspath(os.path.join(args.input_dir, '../faces.tsv'))
     
-    # Default for args.output_file
-    if args.output_file is None:
-        args.output_file = os.path.join(os.path.dirname(args.input_dir), 'faces_embeddings.npy')
-        
     return args
 
 
 
-def make_embeddings(input_dir, faces_tsv_path, model, normalization):
+def make_embeddings(cropped_faces_dir, faces_csv_path, model, normalization):
     
-    face_paths = load_tsv(faces_tsv_path)['id']
-    face_paths = [os.path.join(input_dir, f"{id_}.png") for id_ in face_paths]
-
-    model = DeepFace.build_model(model)
+    face_paths = utils.load_csv(faces_csv_path)['id']
+    face_paths = [cropped_faces_dir / f"{id_}.png" for id_ in face_paths]
 
     embeddings = []
     for path in tqdm(face_paths, ascii = True, desc = 'Files processed'):
-        e = DeepFace.represent(path, model = model, detector_backend = 'skip', normalization = normalization)
-        embeddings.append(e)
+        result, = DeepFace.represent(path, model_name = model, detector_backend = 'skip', normalization = normalization, anti_spoofing = False)
+        embeddings.append(result['embedding'])
 
     return embeddings
-    
-    
+
 
 
 if __name__ == '__main__':
